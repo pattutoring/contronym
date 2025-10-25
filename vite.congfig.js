f7a83f49071108d0c8cca14172047952d325449f
pattutoring <77590908+pattutoring@users.noreply.github.com>
2025-10-25 17:10:14 -0400

    Update vite.congfig.js

--- vite.congfig.js
+++ vite.congfig.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/contronym/',  // 👈 EXACTLY your repository name, with slashes
+  base: '/contronym/',  // 👈 this MUST match exactly the repo name
 })
 
