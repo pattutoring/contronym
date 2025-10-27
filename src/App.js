22f355170ce1bfba361f8937bf1c3e62725f9a5f
pattutoring <77590908+pattutoring@users.noreply.github.com>
2025-10-26 23:15:12 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ 
+      display: "flex", 
+      justifyContent: "center", 
+      alignItems: "center", 
+      height: "100vh", 
+      backgroundColor: "#111", 
+      color: "#fff", 
+      fontFamily: "sans-serif" 
+    }}>
+      <h1>PAT Tutoring Contronym Game 🚀</h1>
+    </div>
+  );
+}
