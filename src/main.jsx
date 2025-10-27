6eb2f3f20ec8c90a51b815a2945782d4bec9d76d
pattutoring <77590908+pattutoring@users.noreply.github.com>
2025-10-26 22:56:09 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 function ContronymGame() {
   const answer = "sanction"; // ✅ You can change this later
