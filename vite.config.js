8d7625c5d90afe98047e72fc9348f09b82939ddb
pattutoring <77590908+pattutoring@users.noreply.github.com>
2025-10-28 11:29:06 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pattutoring.github.io/' // <-- your GitHub Pages repo name
+  base: '/pattutoring.github.io/'  // <-- Replace with your repo name
 })
+
 
 
 
