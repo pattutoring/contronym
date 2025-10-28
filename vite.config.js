932c7dfd2e0acbd3f492a58e2f02670d1d98300d
pattutoring <77590908+pattutoring@users.noreply.github.com>
2025-10-28 10:37:32 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// change ONLY this line if your repo name is different
-const repoName = "contronym";
-
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`,
+  base: "/",
 });
