4de6ee15a4af30b3426cbd6906f479bc71cad4e0
pattutoring <77590908+pattutoring@users.noreply.github.com>
2025-10-28 12:50:17 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/contronym/', // MUST match repo name exactly
+  base: './', // relative base fixes blank page
 })
 
 
